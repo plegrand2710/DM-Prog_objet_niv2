c0d79f6082a4db9cda00f564bd2276bb75592e11
plegrand2710 <pauline.legrand2710@gmail.com>
2025-03-12 18:42:47 -1000

    Board - gestion comportement boss

--- CursedChronicles/src/cursed_chronicles/Monster/ChasePlayerStrategy.java
+++ CursedChronicles/src/cursed_chronicles/Monster/ChasePlayerStrategy.java
@@ -3,17 +3,22 @@ package cursed_chronicles.Monster;
 import javax.swing.*;
 
 import cursed_chronicles.Map.Room;
+import cursed_chronicles.Constant;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ChasePlayerStrategy implements MovementStrategy {
-    private final int stepSize = 8;
-    private final int baseMoveSpeed = 250;
+    private final int baseMoveSpeed = 500; 
+    private final int pauseTime = 200; 
 
     @Override
     public void move(Monster monster, int playerX, int playerY, Room room) {
-        if (monster.isMoving()) return; 
+        if (monster.isMoving()) return;
+        if (room == null) return;
+
+        int[][] collisions = room.getCollisionsLayer();
+        if (collisions == null) return;
 
         int dx = Integer.compare(playerX, monster.getPositionX());
         int dy = Integer.compare(playerY, monster.getPositionY());
@@ -21,31 +26,45 @@ public class ChasePlayerStrategy implements MovementStrategy {
         int targetX = monster.getPositionX() + dx;
         int targetY = monster.getPositionY() + dy;
 
-        if (detectCollision(targetX, targetY)) return;
+        if (!isValidMove(targetX, targetY, room)) {
+            startPauseBeforeNextMove(monster, playerX, playerY, room);
+            return;
+        }
 
         monster.setMoving(true);
-        Timer movementTimer = new Timer(baseMoveSpeed / 10, new ActionListener() {
-            int steps = 0;
 
+        Timer movementTimer = new Timer(baseMoveSpeed, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (steps < 6) {
-                    monster.setPositionX(monster.getPositionX() + dx * stepSize);
-                    monster.setPositionY(monster.getPositionY() + dy * stepSize);
-                    steps++;
-                } else {
-                    ((Timer) e.getSource()).stop();
-                    monster.setPositionX(targetX);
-                    monster.setPositionY(targetY);
-                    monster.setMoving(false);
-                }
+                monster.setPositionX(targetX);
+                monster.setPositionY(targetY);
+                ((Timer) e.getSource()).stop();
+                monster.setMoving(false);
+                startPauseBeforeNextMove(monster, playerX, playerY, room);
             }
         });
 
+        movementTimer.setRepeats(false);
         movementTimer.start();
     }
 
-    private boolean detectCollision(int x, int y) {
-        return false; 
+    private boolean isValidMove(int x, int y, Room room) {
+        int[][] collisions = room.getCollisionsLayer();
+        if (collisions == null) return false;
+
+        if (x < 0 || y < 0 || x >= collisions[0].length || y >= collisions.length) {
+            return false; 
+        }
+
+        return collisions[y][x] != Constant.WALL_COLLISION_ID; 
+    }
+
+    private void startPauseBeforeNextMove(Monster monster, int playerX, int playerY, Room room) {
+        Timer pauseTimer = new Timer(pauseTime, e -> {
+            ((Timer) e.getSource()).stop();
+            move(monster, playerX, playerY, room);
+        });
+        pauseTimer.setRepeats(false);
+        pauseTimer.start();
     }
 }
